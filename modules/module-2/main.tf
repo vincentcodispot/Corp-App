776cbbf90cb3433e9e38ce04cf1ad56752b7c453
vincentcodispot <vincent.codispot@sentinelone.com>
2026-03-05 18:32:16 -0500

    Update main.tf

--- modules/module-2/main.tf
+++ modules/module-2/main.tf
@@ -85,7 +85,7 @@ resource "aws_security_group" "ecs_sg" {
   }
 }
 
-# Create Database for Subnet Group
+# Create Database Subnet Group
 # terraform aws db subnet group
 resource "aws_db_subnet_group" "database-subnet-group" {
   name        = "database subnets"
