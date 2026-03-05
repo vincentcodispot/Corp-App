c35507392bd54e5dbc7d21676f15ce437cf06b97
vincentcodispot <vincent.codispot@sentinelone.com>
2026-03-05 12:21:44 -0500

    Update main.tf

--- modules/module-2/main.tf
+++ modules/module-2/main.tf
@@ -17,7 +17,7 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-# VPC Config for public access
+# VPC Config public access
 resource "aws_vpc" "lab-vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
